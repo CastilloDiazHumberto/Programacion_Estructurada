2cdf4ba0d7b934400f9a7599e576902c7e1105ac
CastilloDiazHumberto <humberto_3041240480@utd.edu.mx>
2025-07-24 18:49:17 -0600

    Add files via upload

--- P3/4_proyecto_notas/usuarios/usuario.py
+++ P3/4_proyecto_notas/usuarios/usuario.py
@@ -1,27 +1,32 @@
-from conexionBD import
+from conexionBD import *
 import datetime
+import hashlib
 
-def registrar(nombre,apellido,email.contraseña):
+def hash_password(contrasena):
+    return hashlib.sha256(contrasena.encode()).hexdigest()
+
+def registrar(nombre,apellidos,email,contrasena):
     try:
-        sql="insert into usuarios (nombre,apellido,email,password,fecha) values (%5, %5, %5, %5, %5,)"
-        val=()
-        cursor.execute(aql,val)
-        conexion.commit()
-        return True
+       fecha=datetime.datetime.now()
+       contrasena=hash_password(contrasena)
+       sql="insert into usuarios (nombre,apellidos,email,password,fecha) values (%s,%s,%s,%s,%s)"
+       val=(nombre,apellidos,email,contrasena,fecha)
+       cursor.execute(sql,val)
+       conexion.commit()
+       return True
     except:
-        return false
-
-
-    def iniciar_sesion(email,contraseña):
-        try:
-            sql="select * from usuarios where email=%5 and password=%5"
-            val=(email,contraseña)
-            cursor.execute(sql,val)
-            registro=cursor.fetchone()
-            if registro:
-                return registro
-            else:
-                return None
-
-        except:
-            return None
+        return False    
+    
+def iniciar_sesion(email,contrasena):
+    try:
+       contrasena=hash_password(contrasena)
+       sql="select * from usuarios where email=%s and password=%s"
+       val=(email,contrasena)
+       cursor.execute(sql,val)
+       registro=cursor.fetchone()
+       if registro:
+           return registro
+       else:
+           return None
+    except:
+        return None    
